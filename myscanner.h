da42ea57fda405b460cfb4f17f2a1ae55be718fc
Ariana B <bermudezarii@gmail.com>
2017-03-12 01:09:40 -0600

    Add files via upload

---
+++ myscanner.h
@@ -0,0 +1,12 @@
+#define KEYWORD 1
+#define INTEGER 2
+#define IDENTIFIER 3
+#define CONSTANT 4
+#define OPERATOR 5
+#define PUNTUACTOR 6
+
+
+
+
+//typedef enum tokens {IDENTIFIER, INTEGER} token;
+
