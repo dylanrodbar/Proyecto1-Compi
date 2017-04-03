a6f0f8b0d52c534bd63ea212b71a6fcf15ed7834
Xbolanos <xbolanosfonseca@gmail.com>
2017-04-02 21:04:58 -0600

    Pruebitas

--- Proyecto2/Codigos de prueba.c
+++ Proyecto2/Codigos de prueba.c
@@ -216,3 +216,141 @@ bool holaFuncion()
 
 
 //*****************************Quinta Prueba***********************************
+//*****************************Sexta Prueba***********************************
+
+//hola.c
+#include "prueba.h"
+#define x 10 + 20
+#define a 5
+#define b a + 6 + 3
+#define a 20 
+#define c 30 + a + b + 10
+#define a 40 + x
+
+int funcionPruebaSegunda(){
+	printf("Prueba") ;
+	int t = c ;
+}
+
+
+
+//prueba.h
+#include "prueba1.h"
+
+//hola
+int funcion()
+{
+   /*printf("Esto es una función");
+   return 0;*/
+}
+
+
+
+//prueba1.h
+#define hola 10 + 3
+#define adios 5 + hola + 5
+#define hi   30 - adios
+
+bool holaFuncion()
+{
+    int a = hola ;
+    int b = hola ;
+    printf("Hello, its me @@@@@");
+    return true;
+}
+
+
+
+//*****************************Sexta Prueba***********************************
+//*****************************Setima Prueba***********************************
+
+//hola.c
+#include "prueba.h"
+#define x 10 + 20
+#define a 5
+#define b a + 6 + 3
+#define a 20 
+#define c 30 + a + b + 10
+#define a 40 + x
+
+int funcionPruebaSegunda(){
+	printf("Prueba") ;
+	int t = c ;
+}
+
+
+
+//prueba.h
+#include "prueba1.h"
+
+//hol@@@
+int funcion()
+{
+   /*printf("Esto es una función");
+   return 0;*/
+}
+
+
+
+//prueba1.h
+#define hola 10 + 3
+#define adios 5 + hola + 5
+#define hi   30 - adios
+
+bool holaFuncion()
+{
+    int a = hola ;
+    int b = hola ;
+    printf("Hello, its me");
+    return true;
+}
+
+
+
+//*****************************Setima Prueba***********************************
+//*****************************Octava Prueba***********************************
+
+//hola.c
+#include "prueba.h"
+#define x 10 + 20
+#define a 5
+#define b a + 6 + 3
+#define a 20 
+#define c 30 + a + b + 10
+#define a 40 + x
+
+int funcionPruebaSegunda(){
+	printf("Prueba") ;
+	int t = c ;
+}
+
+
+
+//prueba.h
+#include "prueba1.h"
+
+//hola
+int funcion()
+{
+   /*printf("Esto es una función");
+   return 0;*/@ #
+}
+
+
+
+//prueba1.h
+#define hola 10 + 3
+#define adios 5 + hola + 5
+#define hi   30 - adios
+
+bool holaFuncion()
+{
+    int a = hola ;
+    int b = hola ;
+    printf("Hello, its me");
+    return true;
+}
+
+
+
+//*****************************Octava Prueba***********************************
